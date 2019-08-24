cb759bca41cdedcf494146eedac5cf5e8122c5f2
xzrlovedy <3056906329@qq.com>
2019-08-24 10:18:39 +0800

    pojo实体类

---
+++ airplane/src/com/pojo/Airport.java
@@ -0,0 +1,25 @@
+package com.pojo;
+
+public class Airport {
+	private int id;
+	private String portName;
+	private String cityName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPortName() {
+		return portName;
+	}
+	public void setPortName(String portName) {
+		this.portName = portName;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+}
